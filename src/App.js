63166a2c838084e61fcc986c8aa1d6f07961aa5c
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-07-30 15:27:14 -0700

    add function to make a request to the server

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+
 
 function App() {
+
+  const apiCall = () => {
+    axios.get('http://localhost:8080').then((data) => {
+      console.log(data);
+    })
+  }
+
+  apiCall();
+  
+  // const [backendData, setBackendData] = useState();
+  // useEffect(() => {
+  //   fetch('/api')
+  //   .then(res => res.json())
+  //   .then(data => setBackendData(data))
+  // }, [])
+  // const display = backendData.map(data => console.log(data))
+
   return (
     <div className="App">
-     
+      App
     </div>
   );
 }
