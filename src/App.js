f592a080dc829d8b611d74c529d8d7f2df20fb73
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-07-30 22:17:06 -0700

    add form for searching based on user's input

--- src/App.js
+++ src/App.js
@@ -3,28 +3,54 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 
-
 function App() {
 
+  const EDAMAM_API_ID = '805a07f7';
+  const EDAMAM_API_KEY = '48484be0535a5017a5e879330aa66712';
+
+  const [input, setInput] = useState('');
+  console.log(input);
+  
+  // test get request based on a search
+  // axios.get('https://api.edamam.com/search', {
+  //   params: {
+  //     q: 'corn',
+  //     app_id: EDAMAM_API_ID,
+  //     app_key: EDAMAM_API_KEY,
+  //   },
+  // })
+  // .then(response => {
+  //   console.log(response.data);
+  // })
+  // .catch(error => {
+  //   console.error('Error fetching data from Edamam API', error);
+  // });
+
+
+  
   const apiCall = () => {
     axios.get('http://localhost:8080').then((data) => {
       console.log(data);
     })
   }
-
-  apiCall();
   
-  // const [backendData, setBackendData] = useState();
-  // useEffect(() => {
-  //   fetch('/api')
-  //   .then(res => res.json())
-  //   .then(data => setBackendData(data))
-  // }, [])
-  // const display = backendData.map(data => console.log(data))
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('hitting submit')
+    setInput('');
+  }
+  
 
   return (
-    <div className="App">
+    <div className='App'>
       App
+      <form onSubmit={handleSubmit}>
+        <label>Search:
+          <input type='text' name='input' value={input} onChange={(e) => setInput(e.target.value)}/>
+        </label>
+        <input type='submit' />
+      </form>
     </div>
   );
 }
