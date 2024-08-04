96d658a922e29c88f246f3497c2466b49cefab39
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-08-04 14:06:36 -0700

    render the list of nutrients in the list cards

--- src/components/FoodCard.js
+++ src/components/FoodCard.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
 const FoodCard = ({ food }) => {
-  return (
+  
+    return (
     <div className='foodCard'>
-        <p>{food.name}</p>
+        <h3>{food.name}</h3>
         <p>Calories: {food.calories}</p>
         <img src={food.image} alt='' />
+        <p>Nutrient values:</p>
+        {food.nutrients.map((n, index) => (
+            <li key={index}>{n}</li>)
+        )}
   </div>
   )
 }
