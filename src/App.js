1429e1ed28333524fad86d89d64eef9dca538303
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-08-04 14:26:52 -0700

    remove comments

--- src/App.js
+++ src/App.js
@@ -6,27 +6,9 @@ import FoodCard from './components/FoodCard';
 
 function App() {
 
-  // const EDAMAM_API_ID = '';
-  // const EDAMAM_API_KEY = '';
-
   const [ingredients, setIngredients] = useState('');
   const [foods, setFoods] = useState([]);
 
-  // test get request based on a search
-  // axios.get('https://api.edamam.com/search', {
-  //   params: {
-  //     q: 'corn',
-  //     app_id: EDAMAM_API_ID,
-  //     app_key: EDAMAM_API_KEY,
-  //   },
-  // })
-  // .then(response => {
-  //   console.log(response.data);
-  // })
-  // .catch(error => {
-  //   console.error('Error fetching data from Edamam API', error);
-  // });
-  
   useEffect(() => {
     axios.get('/api')
     .then((data) => {
@@ -39,10 +21,7 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault();
     axios.post('/api/search', {ingredients})
-    .then(res => {
-      // console.log(res)
-      setFoods(res.data);
-    })
+    .then(res => setFoods(res.data))
     .catch(err => console.log(err))
     setIngredients('');
   }
